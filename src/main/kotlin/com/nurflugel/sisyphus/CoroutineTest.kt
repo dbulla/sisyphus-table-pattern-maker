297ec4e0e79a613bdb9c1822ed9c27b8a87ad2b6
Douglas Bullard <dbullard@nurflugel.com>
2019-08-02 09:07:31 -0700

    Expanding corouting tests

--- src/main/kotlin/com/nurflugel/sisyphus/CoroutineTest.kt
+++ src/main/kotlin/com/nurflugel/sisyphus/CoroutineTest.kt
@@ -16,31 +16,78 @@ class CoroutineTest {
         }
     }
 
+    //    val numberIterations = 1L
+    val numberIterations = 10L                // 6 s
+    //    val numberIterations = 100L               // 7.122S s
+    //    val numberIterations = 1_000L             // 7.19 s
+    //    val numberIterations = 10_000L            // 7.32 s
+    //    val numberIterations = 100_000L           // 8.15 s
+    //    val numberIterations = 1_000_000L         // 16.9 s
+
     fun doIt() {
         val start = Instant.now()
-        println("Starting test")
+
+        println("Starting test with $numberIterations iterations")
+        val sum = runBlocking {
+            println("in sum2")
+            val sum: Int = (0L until numberIterations)
+                .map { GlobalScope.async { funC(it) } }
+                .map { it.await() } // this returns a value which we will sum
+                .sum()
+
+            println("In launch scope")
+            // these run in the order they're declared
+            //            val jobA = GlobalScope.async { funC(3) }
+            //            val jobB = GlobalScope.async { funC(5) }
+            //            val jobC = GlobalScope.async { funC(2) }
+            //            sum = jobB.await() + jobA.await() + jobC.await()
+            //            println("done with launch scope")
+
+            // which we will sum
+
+            sum
+        }
+        println("sum is $sum, total duration for $numberIterations coroutines was " + Duration.between(start, Instant.now()))
+
+    }
+
+    /** Delay function which sleeps and returns the delay in sec */
+    private suspend fun funC(index: Long): Int {
+        val start = Instant.now()
+        println("fun $index start $start")
+        val delay = Math.random() * 5 + 2
+        val timeMillis = (delay * 1000L).toLong()
+        delay(timeMillis)
+        println("fun $index end, duration was " + Duration.between(start, Instant.now()))
+        return delay.toInt()
+    }
+
+    fun doItWithThreads() {
+        val start = Instant.now()
+
+        println("Starting test with $numberIterations iterations")
         val sum = runBlocking {
             println("in sum2")
             var sum = 0
 
             println("In launch scope")
             // these run in the order they're declared
-            val jobA = GlobalScope.async { funC(3) }
-            val jobB = GlobalScope.async { funC(5) }
-            val jobC = GlobalScope.async { funC(2) }
-            sum = jobB.await() + jobA.await() + jobC.await()
-            println("done with launch scope")
+            //            val jobA = GlobalScope.async { funC(3) }
+            //            val jobB = GlobalScope.async { funC(5) }
+            //            val jobC = GlobalScope.async { funC(2) }
+            //            sum = jobB.await() + jobA.await() + jobC.await()
+            //            println("done with launch scope")
+
+            sum = (0 until numberIterations)
+                .map { GlobalScope.async { funC(it) } }
+                .map { it.await() }
+                .sum()
+
             sum
         }
-        println("sum is $sum, total duration was " + Duration.between(start, Instant.now()))
+        println("sum is $sum, total duration for $numberIterations threads was " + Duration.between(start, Instant.now()))
 
     }
 
-    private suspend fun funC(delay: Int): Int {
-        val start = Instant.now()
-        println("fun $delay start")
-        delay(delay * 1000L)
-        println("fun $delay end, duration was " + Duration.between(start, Instant.now()))
-        return delay
-    }
+
 }
